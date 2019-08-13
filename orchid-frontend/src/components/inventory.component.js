49ef8569a5df958479bfa24b731b4ad6fb62bcd7
Arsen <arsenohanyan@gmail.com>
2019-08-13 15:04:10 -0700

    Listing Buttons Added

--- orchid-frontend/src/components/inventory.component.js
+++ orchid-frontend/src/components/inventory.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import SpinnerComponent from './spinner.component';
+import '../stylesheets/listing.scss';
 
 export default class InventoryComponent extends React.Component{
     constructor(props){
@@ -23,24 +24,52 @@ export default class InventoryComponent extends React.Component{
     componentDidMount(){
         axios.get('http://localhost:3001/inventory/').then((response) => {
             if(response.status === 200){
-                this.setState({products: response.data, fetching: false});
+                var prods = response.data;
+                prods.forEach(element => {
+                    element.checked = false;
+                });
+                this.setState({products: prods, fetching: false});
             }
             }).catch(err => {
                 this.setState({products: [], fetching: false});
           });
     }
 
+    toggleCheck(){
+
+    }
+
     render(){
     return (this.state.fetching? <SpinnerComponent /> : (
         <div className="inventory-container">
-
-        </div>
+            <ul className="button-list">
+            {this.state.products.map((val)=><li><ProductButtonComponent>{val.name} {val.sku} {val.price}</ProductButtonComponent></li>)}
+            </ul>
+            </div>
     ));
     }
 }
 
 function ProductButtonComponent(props){
-
+    function handleCheck(e){
+        e.stopPropagation();
+    }
+    function handleMouseDown(e){
+        e.target.className = 'listing-button clicked';
+    }
+    function handleMouseUp(e){
+        e.target.className = 'listing-button';
+    }
+    return(
+        <div 
+        className="listing-button" 
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}>
+        <input type="checkbox" onClick={handleCheck} onMouseDown={handleCheck} onMouseUp={handleCheck}/>
+        <h1></h1>
+        {props.children}
+        </div> 
+    );
 }
 
 function ProductDetailComponent(props){
