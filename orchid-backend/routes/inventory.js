2e489febe77dac0d51f0473afe5a6fad4e3d0eaf
Arsen <arsenohanyan@gmail.com>
2019-08-25 22:50:53 -0700

    Ignoring images

--- orchid-backend/routes/inventory.js
+++ orchid-backend/routes/inventory.js
@@ -11,7 +11,7 @@ var fs = require('fs');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-    cb(null, 'public')
+    cb(null, 'public/images')
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' +file.originalname )
