91343d01a955e3ee3abe2745ec0ef8b6a4a4e189
Arsen Ohanyan <arsenohanyan@gmail.com>
2020-06-13 14:39:47 -0700

    Backend-only branch

--- orchid-frontend/src/components/spinner.component.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../stylesheets/spinner.scss';
-export default function SpinnerComponent(props){
-
-   return <div className = "spinner-container">
-       <div className = "loader">Loading...</div>
-   </div>
-}
